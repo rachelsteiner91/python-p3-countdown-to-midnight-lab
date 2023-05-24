83e8fae794c16b9a1f9fb83177f49f4ab435bca8
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-05-24 14:06:03 -0400

    countdown to midnight complete

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,6 +1,20 @@
 # your code goes here!
-def countdown(arg):
-    print(arg)
 
-def countdown_with_sleep(arg):
-    print(arg)
+#Function countdown() in countdown.py counts down from number and prints "HAPPY NEW YEAR!" - AssertionError: assert '5\n' == '5 SECOND(S)!...Y NEW YEAR!\n'
+
+def countdown(start_countdown):
+    # start_countdown = 5
+    while start_countdown > 0:
+        print(f'{start_countdown} SECOND(S)!')
+        start_countdown -= 1
+    print("HAPPY NEW YEAR!")
+countdown(5)
+
+
+def countdown_with_sleep(start_countdown):
+     # start_countdown = 5
+    while start_countdown > 0:
+        print(f'{start_countdown} SECOND(S)!')
+        start_countdown -= 1
+    print("HAPPY NEW YEAR!")
+countdown(5)
